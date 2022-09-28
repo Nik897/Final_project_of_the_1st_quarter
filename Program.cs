67844ca2c2eba26c5e2365ec96f7e177c2fb8e22
Nik <nik@yahoo.com>
2022-09-28 20:41:00 +0700

    Добавил программу

--- Program.cs
+++ Program.cs
@@ -1,1 +1,34 @@
-﻿System.Console.WriteLine("Test");
+﻿/* Написать программу, которая изимеющегося массива строк формирует массив изстрок,
+длинна которых меньше либо равно 3. первоначальный массив можно ввести с клавиатуры,
+либо задать на старте выполнения алгоритма. 
+*/
+
+Console.Clear();
+
+string[] initialArray = { "123", "test", "London", "Q", ":-)" };
+
+string[] resultArray = new string[initialArray.Length];
+
+printArray(NewArray(initialArray));
+
+void printArray(string[] array) // Выводим массив на экран
+{
+    System.Console.WriteLine(string.Join(" ", array));
+}
+
+string[] NewArray(string[] array) // Создаем новый массив, в котором длинна строк меньши или равна 3 в исходном массиве
+{
+    int j = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        string temp = array[i];
+
+        if (temp.Length <= 3)
+        {
+            resultArray[j] = temp;
+            j++;
+        }
+    }
+    return resultArray;
+}
